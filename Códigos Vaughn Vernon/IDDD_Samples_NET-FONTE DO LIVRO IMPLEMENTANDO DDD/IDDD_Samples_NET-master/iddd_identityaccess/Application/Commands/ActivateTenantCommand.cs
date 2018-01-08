6956c959703ee84fead66a05626b4c0b2bf57228
Marcos.Baiao <marcos@onlytechsolutions.com.br>
2018-01-08 16:34:52 -0200

    Novos códigos úteis coletados!

---
+++ Códigos Vaughn Vernon/IDDD_Samples_NET-FONTE DO LIVRO IMPLEMENTANDO DDD/IDDD_Samples_NET-master/iddd_identityaccess/Application/Commands/ActivateTenantCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nucleus.Validacoes;
+
+namespace SaaSOvation.IdentityAccess.Application.Commands
+{
+    public class ActivateTenantCommand
+    {
+        public ActivateTenantCommand() { }
+
+        public ActivateTenantCommand(string tenantId)
+        {
+            this.TenantId = tenantId;
+        }
+
+        public string TenantId { get; set; }
+    }
+}
