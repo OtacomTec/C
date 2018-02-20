e803684156905a87a0742bea6b3871bea8e62172
Marcos.Baiao <marcos@onlytechsolutions.com.br>
2018-02-20 09:47:30 -0300

    Comiit com testes

---
+++ Códigos balta.io/1974 - Criando APIs com ASP.NET Core 2.0 e Dapper/1974-master/BaltaStore.Api/Startup.cs
@@ -0,0 +1,53 @@
+﻿using BaltaStore.Domain.StoreContext.Handlers;
+using BaltaStore.Domain.StoreContext.Repositories;
+using BaltaStore.Domain.StoreContext.Services;
+using BaltaStore.Infra.DataContexts;
+using BaltaStore.Infra.StoreContext.Repositories;
+using BaltaStore.Infra.StoreContext.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using Elmah.Io.AspNetCore;
+using System;
+
+namespace BaltaStore.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddResponseCompression();
+
+            services.AddScoped<BaltaDataContext, BaltaDataContext>();
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddTransient<CustomerHandler, CustomerHandler>();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Info { Title = "Balta Store", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseMvc();
+            app.UseResponseCompression();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Balta Store - V1");
+            });
+
+            app.UseElmahIo("923f4c946cc1435cb0ec665d6e7370b7", new Guid("e42a9995-df89-4d91-a625-ecc57d124004"));
+        }
+    }
+}
